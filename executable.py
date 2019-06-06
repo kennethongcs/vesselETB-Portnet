9d975d430ff70691b2d30edaed9f179d5690f62b
kocs <kenneth.ongcs@gmail.com>
2019-06-06 21:34:03 +0800

    update 2

--- executable.py
+++ executable.py
@@ -1,2 +1,14 @@
-#main code
-#more things to do here
+import urllib2
+from bs4 import BeautifulSoup
+
+# specify the url
+quote_page = "https://www.portnet.com/login"
+
+# query the website and return the html to the variable ‘page’
+page = urllib2.urlopen(quote_page)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page, ‘html.parser’)
+
+# Take out the <div> of name and get its value
+name_box = soup.find(‘h1’, attrs={‘class’: ‘name’})
