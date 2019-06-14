ee9e3de0a14cf2cf32b1afbc45a6408b2bb10ce1
Kenneth Ong <kenneth@SAMUDERA.COM>
2019-06-14 17:52:39 +0800

    update 2

--- etb.py
+++ etb.py
@@ -24,8 +24,10 @@ password.clear
 password.send_keys('Welcome1')
 
 driver.find_element_by_name('Login').click()
-
-driver.find_element_by_xpath('//*[@id="Login"]/table/tbody/tr[9]/td/p/input').click()
+try:
+    driver.find_element_by_xpath('//*[@id="Login"]/table/tbody/tr[9]/td/p/input').click()
+except:
+    print('No token needed')
 
 assert 'No results found.' not in driver.page_source
 print('*** You are in',driver.title,'***')
@@ -36,7 +38,7 @@ driver.switch_to_window(window_main)
 driver.switch_to_frame('Main')
 driver.find_element_by_partial_link_text('VEDA').click()
 
-driver.implicitly_wait(5) # seconds
+#driver.implicitly_wait(5) # seconds
 
 # finding vessel ETB
 vslName = driver.find_element_by_name('{actionForm.vslName}')
@@ -47,16 +49,33 @@ driver.find_element_by_xpath('/html/body/form/table[1]/tbody/tr/td/table[2]/tbod
 driver.implicitly_wait(5) # seconds
 
 # entered ETB URL
-window_etb = driver.window_handles[1]
+window_etb = driver.window_handles[0]
 
-page = requests.get(driver.current_url).text
+page = requests.get(driver.current_url)
 # USING SOUP
-soup = BeautifulSoup(page,'lxml')
-my_table = soup.find('table',{'border':'0'})
-links = my_table.findAll('a')
-data = []
-for link in links:
-    data.append(link.get('title'))
+soup = BeautifulSoup(page,'html.parser')
+frames = soup.findAll('frame',{'name':'main'})
+# my_table = soup.find('table',{'border':'0'})
+links = my_table.findAll('tr',{'class'})
+print (links)
+# header = []
+# data = []
+# for link in links:
+#     header.append(link.get('title'))
+# for m in soup.findAll('tr'):
+#     data.append(m)
+
+# with open('Sample.csv', 'w') as csvfile:
+#       writer = csv.DictWriter(csvfile, fieldnames=header)
+#       writer.writeheader()
+#       tempDateList = []
+#       DateList = []
+#       for i in data:
+#         tempDateList.append(i["Date"])
+#       for i in tempDateList:
+#         DateList.append(datetime.datetime.strptime(i, "%d-%b-%Y").strftime("%Y-%m-%d"))
+#       for i in data:
+#         writer.writerow(i)
 
 # USING LXML
 # doc = lh.fromstring(page)
@@ -64,14 +83,14 @@ for link in links:
 
 #Create empty list
 
-# col=[]
-# i=0
-#For each row, store each first element (header) and an empty list
+col=[]
+i=0
+# For each row, store each first element (header) and an empty list
 
-# for t in tr_elements[0]:
-#     i+=1
-#     name=t.text_content()
-#     print ('%d:"%s"'%(i,name))
-#     col.append((name,[]))
+for t in links[0]:
+    i+=1
+    name=t.text_content()
+    print ('%d:"%s"'%(i,name))
+    col.append((name,[]))
 
 # driver.quit()
